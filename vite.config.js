0bd100b746cebd262569b83f95fa7d0e72d4d688
afasari <batiar.rahmamulia@gmail.com>
2023-10-19 23:55:51 +0700

    update action

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './vue-workspace/',
   plugins: [vue()],
   server: {
     watch: {
       usePolling: true
     },
-    base: '/vue-workspace/',
   }
 })
