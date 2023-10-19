2d3f70958e21cd79ecb9cd0a61a20035ba9adedb
afasari <batiar.rahmamulia@gmail.com>
2023-10-20 00:04:40 +0700

    update

--- vue-vite-pokedex/vite.config.js
+++ vue-vite-pokedex/vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue-workspace/',
+  base: '/vue-workspace/vue-vite-pokedex',
   plugins: [vue()],
   server: {
     watch: {
